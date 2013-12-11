d1644795c9417b8f42f64bb2c206736f6d1a2f3a
wuzhimin1989 <wzm198965@gmail.com>
2013-12-11 21:42:00 +0800

    2013.12.11 state

---
+++ State.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using PAT.Common.Classes.Expressions.ExpressionClass;
+namespace PAT.LTS.LTS
+{
+    public class State
+    {
+        public List<Transition> OutgoingTransitions;
+        public string Name;
+        /// <summary>
+        /// Integer type
+        /// </summary>
+        public string ID;
+
+        public State(string name, string id)
+        {
+            Name = name;
+            ID = id;
+            OutgoingTransitions = new List<Transition>();
+        }
+
+        public State ClearConstant(Dictionary<string, Expression> constMapping)
+        {
+
+            return new State(Name, ID);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
